2447f53efe71418820b16ab68a55642ed7d5f0bc
Axel281296 <39245301+Axel281296@users.noreply.github.com>
2019-01-22 23:57:40 -0500

    ARDUINO

---
+++ INTERMEDIO/LASER_MODULE/LASER_MODULE.ino
@@ -0,0 +1,20 @@
+
+int pos = 0;    
+
+void setup() {
+  pinMode(9,OUTPUT);  
+}
+
+void loop() {
+  for (pos = 0; pos <= 255; pos += 1) 
+  { 
+
+    analogWrite(9,pos);              
+    delay(25);                       
+  }
+  for (pos = 255; pos >= 0; pos -= 1) 
+  { 
+    analogWrite(9,pos);             
+    delay(25);                      
+  }
+}
